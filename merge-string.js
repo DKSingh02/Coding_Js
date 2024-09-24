8b71914e1db4f461869356110ffedba3c0a72ede
Diwakar Kumar <diwakar@MacBook-Pro.lan>
2024-09-24 23:49:39 +0530

    merge string alternatively solution

---
+++ merge-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+ const mergeAlternately = function(word1, word2) {
+
+    let mergedString = [];
+    let maxLength = Math.max(word1.length, word2.length);
+    
+    for (let i = 0; i < maxLength; i++) {
+        if (i < word1.length) mergedString.push(word1[i]);
+        if (i < word2.length) mergedString.push(word2[i]);
+    }
+    
+    return mergedString.join('');   
+};
+
+console.log(mergeAlternately("abc", "pqr"));
